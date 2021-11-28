39e6d51c39afeefe4a117f403175a18fb589e977
Maxsman <dekxirus@gmail.com>
2021-11-29 02:02:47 +1000

    Add Terrain Class

---
+++ Terrain.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Digger
+{
+    public class Terrain : ICreature
+    {
+        public CreatureCommand Act(int x, int y)
+        {
+            return new CreatureCommand
+            {
+                DeltaX = 0,
+                DeltaY = 0,
+                TransformTo = null
+            };
+        }
+
+        public bool DeadInConflict(ICreature conflictedObject)
+        {
+            return true;
+        }
+
+        public int GetDrawingPriority()
+        {
+            return 1;
+        }
+
+        public string GetImageFileName()
+        {
+            return "Terrain.png";
+        }
+    }
+}
